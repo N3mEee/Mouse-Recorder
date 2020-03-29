5364d5579113b29a70ab53314964ffebd6638cfa
N3mEee <62804947+N3mEee@users.noreply.github.com>
2020-03-29 16:02:59 +0200

    Upload
    Upload files

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Mouse_Recorder
+{
+    class Program
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+        public static POINT GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+            return lpPoint;
+        }
+
+        static void Main(string[] args)
+        {
+            var timer = new System.Threading.Timer(
+                e =>
+                {
+                    var p = GetCursorPosition();
+                    Console.WriteLine(string.Format("X: {0}, Y: {1}", p.X, p.Y));
+                },
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromSeconds(0.1));
+
+            Console.ReadLine();
+        }
+    }
+}
